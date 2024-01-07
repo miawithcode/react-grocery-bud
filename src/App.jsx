ca982f4e51a6310954933113fe73925aec0a563d
miawithcode <cuttontail@gmail.com>
2024-01-07 13:13:30 +0800

    feat: addItem

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import { useState } from "react"
-import Form from "./components/Form";
+import { useState } from 'react';
+import Form from './components/Form';
+import { nanoid } from 'nanoid';
 
 const App = () => {
   const [items, setItems] = useState([]);
 
+  const addItem = (item) => {
+    const newItem = {
+      name: item,
+      completed: false,
+      id: nanoid(),
+    };
+    const newItems = [...items, newItem];
+    setItems(newItems);
+  };
+
   return (
     <section>
-      <Form />
+      <Form addItem={addItem} />
     </section>
-  )
-}
-export default App
+  );
+};
+export default App;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,21 +1,27 @@
-import { useState } from "react"
+import { useState } from 'react';
 
-const Form = () => {  
+const Form = ({ addItem }) => {
   const [newItem, setNewItem] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(newItem);
-  }
+    if(!newItem) return;
+    addItem(newItem);
+    setNewItem('');
+  };
 
   return (
     <form onSubmit={handleSubmit}>
       <h4>Grocery Bud</h4>
       <div>
-        <input type="text" value={newItem} onChange={(e)=>setNewItem(e.target.value)} />
+        <input
+          type="text"
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
+        />
         <button type="submit">Add item</button>
       </div>
     </form>
-  )
-}
-export default Form
+  );
+};
+export default Form;
